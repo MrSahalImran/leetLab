c9f88d43981d8d0332c880fa0a2f25a463dead22
sahal <sahalimran.pro@gmail.com>
2025-06-24 15:45:03 +0530

    initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config({
+  path: "../.env",
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("Server is running on port 8000");
+});
